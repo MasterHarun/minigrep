ea288658609ecc8d4261faff36a54d1c2cd3638f
Harun <master.harun@gmail.com>
2021-09-03 14:24:53 -0400

    Changed parse_config into Config::new

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
    
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -15,10 +15,11 @@ struct Config {
     query: String,
     filename: String,
 }
+impl Config {    
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config { query, filename }
+        Config { query, filename }
+    }
 }
